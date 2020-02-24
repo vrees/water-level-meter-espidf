ae8a7c61a1bc26ebe5cff20d0d7f0b599a274908
vrees <viktor.rees@gmx.de>
2020-02-24 14:21:54 +0100

    fixed spi problem

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,20 @@
  * https://github.com/manuelbl/ttn-esp32
  *******************************************************************************/
 
+
 #include "freertos/FreeRTOS.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
-
-
+#include "TheThingsNetwork.h"
 #include "ttn.h"
+
+
+
 
 extern "C" void app_main(void)
 {
 
 
-    esp_err_t err;
-    
-    // Initialize the GPIO ISR handler service
-    err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-    ESP_ERROR_CHECK(err);
-
-    // Initialize the NVS (non-volatile storage) for saving and restoring the keys
-    err = nvs_flash_init();
-    ESP_ERROR_CHECK(err);
-
-    // Initialize SPI bus
-    spi_bus_config_t spi_bus_config;
-    spi_bus_config.miso_io_num = TTN_PIN_SPI_MISO;
-    spi_bus_config.mosi_io_num = TTN_PIN_SPI_MOSI;
-    spi_bus_config.sclk_io_num = TTN_PIN_SPI_SCLK;
-    spi_bus_config.quadwp_io_num = -1;
-    spi_bus_config.quadhd_io_num = -1;
-    spi_bus_config.max_transfer_sz = 0;
-    // spi_bus_config.intr_flags = spi_bus_config.intr_flags | !(ESP_INTR_FLAG_HIGH|ESP_INTR_FLAG_EDGE|ESP_INTR_FLAG_INTRDISABLED);
-    err = spi_bus_initialize(TTN_SPI_HOST, &spi_bus_config, TTN_SPI_DMA_CHAN);
-    ESP_ERROR_CHECK(err);
 
     ttnInit();    
 
--- src/ttn.cpp
+++ src/ttn.cpp
@@ -20,7 +20,7 @@ const char *appKey = "1D48B6FABE11BCC164028B0F0E9F4990";
 #define TTN_PIN_DIO0 26
 #define TTN_PIN_DIO1 33
 
-static TheThingsNetwork ttn;
+TheThingsNetwork ttn;
 
 const unsigned TX_INTERVAL = 30;
 static uint8_t msgData[] = "Hello, world";
@@ -37,12 +37,33 @@ void sendMessages(void *pvParameter)
     }
 }
 
-void ttnInit()
+extern "C" void ttnInit()
 {
+    esp_err_t err;
+    
+    // Initialize the GPIO ISR handler service
+    err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+    ESP_ERROR_CHECK(err);
 
+    // Initialize the NVS (non-volatile storage) for saving and restoring the keys
+    err = nvs_flash_init();
+    ESP_ERROR_CHECK(err);
+
+    // Initialize SPI bus
+    spi_bus_config_t spi_bus_config;
+    spi_bus_config.miso_io_num = TTN_PIN_SPI_MISO;
+    spi_bus_config.mosi_io_num = TTN_PIN_SPI_MOSI;
+    spi_bus_config.sclk_io_num = TTN_PIN_SPI_SCLK;
+    spi_bus_config.quadwp_io_num = -1;
+    spi_bus_config.quadhd_io_num = -1;
+    spi_bus_config.max_transfer_sz = 0;
+    // spi_bus_config.intr_flags = spi_bus_config.intr_flags | !(ESP_INTR_FLAG_HIGH|ESP_INTR_FLAG_EDGE|ESP_INTR_FLAG_INTRDISABLED);
+    err = spi_bus_initialize(TTN_SPI_HOST, &spi_bus_config, TTN_SPI_DMA_CHAN);
+    ESP_ERROR_CHECK(err);
 
     // Configure the SX127x pins
     ttn.configurePins(TTN_SPI_HOST, TTN_PIN_NSS, TTN_PIN_RXTX, TTN_PIN_RST, TTN_PIN_DIO0, TTN_PIN_DIO1);
+
 
     // The below line can be commented after the first run as the data is saved in NVS
     ttn.provision(devEui, appEui, appKey);
